b73bb0d6775b68b446bb62037c385f7fef6133ca
staticWagomU <to6wa17@gmail.com>
2024-10-20 22:18:21 +0900

    chore(deps): add tanstack router

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    plugins: [TanStackRouterVite()],
+  },
 });
